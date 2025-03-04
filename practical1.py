04379620c0e64903b3797245964802322843c2ca
ThinleyTshering <02240103.cst@rub.edu.bt>
2025-03-04 22:22:14 +0530

    My Second commit

--- practical1.py
+++ practical1.py
@@ -1,4 +1,4 @@
-studentName = "Thinley Tshering"
+studentName = "Thinley Tshering 1EE"
 studentNumber = "02240103"
 
 print("My name is " + studentName + " and my student number is " + studentNumber)
